68caa453e344cdf104511766a52ea0057efc360c
Maria <mashka.l.minsk@gmail.com>
2022-09-30 18:58:36 +0300

    Laba 1 & 2 Changes

---
+++ Laba2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Bus
+{
+    public string Name = "Undefined";
+    public int BusNumber;
+    public int RouteNumber;
+    public string BrandName = "Undefined";
+    public byte Year;
+    public int Mileage;
+
+    public void AgePrint()
+    {
+        Console.WriteLine("Возраст автобуса: {0}", (2022 - Year));
+    }
+}
+
+namespace Laba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
